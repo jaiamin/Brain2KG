2e0f42f428b0a793d4ca579287496ad531f2f33b
jaiamin <jaiamin130@gmail.com>
2024-08-01 17:47:29 -0400

    Update define ontology to csv pipeline

--- define_ontology_to_csv.py
+++ define_ontology_to_csv.py
@@ -9,18 +9,21 @@ from brain2kg.text2kg.utils.llm_utils import parse_relation_definition
 
 def define_ontology_relations_to_csv(
         csv_schema_file_path: str,
-        relations: str,
         few_shot_examples_file_path: str,
-        ontology_triplets: str,
+        ontology_triplets_file_path: str,
         model: str = 'llama3.1',
+        detail_log=True,
     ) -> None:
     # generates a CSV in `schemas/` mapping each relation to its formal definition
     
-    relations = relations.split(',')
-    ontology_triplets = ast.literal_eval(ontology_triplets)
+    ontology_triplets_str = open(ontology_triplets_file_path).read()
+    ontology_triplets = ast.literal_eval(ontology_triplets_str)
+    relations = set()
+    for t in ontology_triplets:
+        relations.add(t[1])
 
     PROMPT = """
-    You will be given a list of relational triples in the format of [Subject, Relation, Object] extracted from a predefined ontology. For each relation present in the triples, your task is to write a description to express the meaning of the relation. In your answer, please strictly ONLY INCLUDE the relation and description pairs and DO NOT include any other comments, explanations or apologies.
+    You will be given a list of relational triples in the format of [Subject, Relation, Object] extracted from a predefined neuroscience-domain ontology. For each relation present in the triples, your task is to write a description to express the meaning of the relation. In your answer, please strictly ONLY INCLUDE the relation and description pairs and DO NOT include any other comments, explanations or apologies.
 
     Here are some examples (pay attention to answer structure and format):
     {few_shot_examples}
@@ -42,6 +45,16 @@ def define_ontology_relations_to_csv(
         model=model,
         messages=messages,
     )['message']['content']
+
+    if detail_log:
+        print('---')
+        print('PROMPT:')
+        print(filled_prompt)
+        print('---')
+        print('COMPLETION:')
+        print(completion)
+        print('---')
+
     relation_definition_dict = parse_relation_definition(completion, relations)
     if not relation_definition_dict:
         print('Error parsing response.')
@@ -51,28 +64,27 @@ def define_ontology_relations_to_csv(
         spamwriter = csv.writer(csvfile)
         for relation, relation_definition in relation_definition_dict.items():
             spamwriter.writerow([relation, relation_definition])
+    
+    print('Complete!')
         
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument("--csv_schema_file_path", help="Path to CSV schmea")
-    parser.add_argument("--relations", help="Ontology relations string, separated by commas")
     parser.add_argument("--few_shot_examples_file_path", help='Path to few-shot examples for defining relations')
-    parser.add_argument("--ontology_triplets", help="Triplets list of lists, in string form")
+    parser.add_argument("--ontology_triplets_file_path", help="Path to Triplets list of lists, in string form")
     parser.add_argument("--model", default="llama3.1", help='Ollama LLM to use')
 
     args = parser.parse_args()
 
     csv_schema_file_path = args.csv_schema_file_path
-    relations = args.relations
     few_shot_examples_file_path = args.few_shot_examples_file_path
-    ontology_triplets = args.ontology_triplets
+    ontology_triplets_file_path = args.ontology_triplets_file_path
     model = args.model
 
     define_ontology_relations_to_csv(
         csv_schema_file_path,
-        relations,
         few_shot_examples_file_path,
-        ontology_triplets,
+        ontology_triplets_file_path,
         model
     )
